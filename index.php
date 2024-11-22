309ccb5ec3169e400fcff7e51f221460297a26aa
reg <reg8740223@gmail.com>
2024-11-22 16:25:00 +0800

    物件導向基礎2

--- index.php
+++ index.php
@@ -10,9 +10,10 @@
 <?php
 
 class Animal{    
-public $type='animal';
-public $name='John';
-public  $hair_color='black';
+protected $type='animal';
+protected $name='John';
+protected  $hair_color='black';
+protected $feet=['front-left','front-right','back-left','back-right'];
 
 function _construct($type,$name,$hair_color){
     $this->type=$type;
@@ -26,17 +27,67 @@ echo $this->name.'is running';
 public function speed(){
 echo $this->name.'is running at 20km/h';
 }
+
+public function getName(){
+    return $this->name;
+
+}
+
+public function setName($name){
+     $this->name=$name;
+
+}
 }
 
 // 實例化(instance)
 $cat=new Animal('cat','Kitty','white');
 
-echo $cat->type;
-echo $cat->name;
-echo $cat->hair_color;
+// echo $cat->type;
+echo $cat->getName();
+// echo $cat->hair_color;
 echo $cat->run();
 echo $cat->speed();
+// print_r($cat->feet);
 
+echo $cat->setName('Mary');
+echo $cat->getName();
+?>
+
+<h1>繼承</h1>
+<?php
+
+class Cat extends Animal{
+protected $type='cat';
+protected $name='Judy';
+function __construct($hair_color){
+    $this->hair_color=$hair_color;
+}
+function jump(){
+    echo $this->name . "jumpping 2m";
+}
+
+}
+
+
+interface Behavior{
+public function run();
+ public function speed();
+    public function jump();
+    
+}
+
+
+$mycat=new Cat('white');
+
+echo $mycat->getName();
+echo "<br>";
+echo $mycat->run();
+echo "<br>";
+echo $mycat->speed();
+echo "<br>";
+$mycat->setName("judy");
+echo $mycat->getName();
+echo "<br>";
 
 
 
