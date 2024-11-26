26e9577fcc2557203f9c2aefe22b20e9e143733b
reg <reg8740223@gmail.com>
2024-11-26 15:29:37 +0800

    完成find()函式

--- db.php
+++ db.php
@@ -12,18 +12,46 @@ $this->pdo=new PDO($this->dsn,'root','');
     }
 //     
 // 撈出全部資料
-
+// 1.整張資料表
+// 2.有條件
+// 3.其他SQL功能
 
 
 //     
-    function all(){
-        return $this->q("SELECT * FROM $this->table");
+    function all(...$arg){
+$sql="SELECT * FROM $this->table";
+if(!empty($arg[0])){
+    if(is_array($arg[0])){
+
+        $where=$this->a2s($arg[0]);
+                $sql=$sql . " WHERE ". join(" && ",$where);
+            }else{
+                //$sql=$sql.$arg[0];
+                $sql .= $arg[0];
+    }
+}
+
+if(!empty($arg[1])){
+    $sql=$sql . $arg[1];
+}
+return $this ->fetchAll($sql);
     }
 
+
+    function find($id){
+        $sql="SELECT * FROM $this->table ";
+        if(is_array($id)){
+            $where=$this->a2s($id);
+            $sql=$sql . " WHERE ". join(" && ",$where);
+        }else{
+            $sql .= " WHERE `id`='$id' ";
+        }
+        return $this->fetchOne($sql);
+    }
     // 
     // 把陣列轉成條件字串陣列
     // 
-function toWhere($array){
+function a2s($array){
     $tmp=[];
     foreach($array as $key => $value){
         $tmp[]="`$key`='$value'";
@@ -33,12 +61,12 @@ function toWhere($array){
 
 function fetchOne($sql){
     // echo $sql
-return $this->pdo->query($sql)->fetch();
+return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
 }
 
+function fetchAll($sql){
     // echo $sql
-function fetchAll(){
-    return $this->pdo->query($sql)->fetchAll();
+    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 }
 
 
@@ -57,6 +85,6 @@ function dd($array){
 $DEPT=new DB('dept');
 
 //$dept=$DEPT->q("SELECT * FROM dept");
-$dept=$DEPT->all();
+$dept=$DEPT->find(['code'=>'404']);
 
 dd($dept);
